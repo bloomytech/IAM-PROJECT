d9e8e0d2e729000593702108b3f772d7aee28190
SYLVA1234 <142400531+SYLVA1234@users.noreply.github.com>
2024-06-24 19:38:27 +0100

    Create iam-group-with-policies.tf

---
+++ iam-group-with-policies.tf
@@ -0,0 +1,23 @@
+module "iam_group_with_policies" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-group-with-policies"
+
+  name = "superadmins"
+
+  group_users = [
+    "user1",
+    "user2"
+  ]
+
+  attach_iam_self_management_policy = true
+
+  custom_group_policy_arns = [
+    "arn:aws:iam::aws:policy/AdministratorAccess",
+  ]
+
+  custom_group_policies = [
+    {
+      name   = "AllowS3Listing"
+      policy = data.aws_iam_policy_document.sample.json
+    }
+  ]
+}
