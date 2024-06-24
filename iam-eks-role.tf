f8e2866b1d6e1301bc3e852754229f665d17d9f3
SYLVA1234 <142400531+SYLVA1234@users.noreply.github.com>
2024-06-24 19:26:54 +0100

    Create iam-eks-role.tf

---
+++ iam-eks-role.tf
@@ -0,0 +1,21 @@
+module "iam_eks_role" {
+  source      = "terraform-aws-modules/iam/aws//modules/iam-eks-role"
+
+  role_name   = "my-app"
+
+  cluster_service_accounts = {
+    "cluster1" = ["default:my-app"]
+    "cluster2" = [
+      "default:my-app",
+      "canary:my-app",
+    ]
+  }
+
+  tags = {
+    Name = "eks-role"
+  }
+
+  role_policy_arns = {
+    AmazonEKS_CNI_Policy = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  }
+}
